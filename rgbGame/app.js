a0cad25cf5707365e07df67adb00aa97580aa949
PavanTejaAnne <saipavanteja.anne@sjsu.edu>
2017-06-23 01:36:32 -0700

    Update app.js

--- rgbGame/app.js
+++ rgbGame/app.js
@@ -1,81 +1,78 @@
-//alert("Check Conn");
+//alert("Check Connection");
 
+var mode = 6;
+var colors=[];
+var picked;
 var topBlock = document.querySelector(".topBlock");
 var block = document.querySelectorAll(".block");
 var colorPicker = document.querySelector("#colorPicker");
 var gameStatus = document.getElementById("status");
 var reset = document.getElementById("reset");
-var easy = document.getElementById("easymode");
-var hard = document.getElementById("hardmode");
-var mode = 6;
-var colors=colorArray(mode);
+var diffMode = document.querySelectorAll(".diffmode");
 
-var picked = pickColor();
-colorPicker.textContent = picked;
+init();
+
+function init(){
+    difficultylevel();
+    arrayBlock();
+    restart();
+}
 
 
 reset.addEventListener("click", function(){
-    colors=colorArray(mode);
-    picked = pickColor();
-    colorPicker.textContent = picked;
-    for(var i = 0; i<block.length; i++){
-    block[i].style.backgroundColor = colors[i];
-    }
-    gameStatus.textContent = "";
-    reset.textContent = "New Colors";
-    topBlock.style.backgroundColor = "cadetblue";
+    restart();
 });
 
-easy.addEventListener("click", function(){
-    hard.classList.remove("difficulty");
-    easy.classList.add("difficulty");
-    mode = 3;
+function difficultylevel(){
+     for(var i = 0; i<diffMode.length; i++){
+        diffMode[i].addEventListener("click",function(){
+                diffMode[0].classList.remove("difficulty");
+                diffMode[1].classList.remove("difficulty");
+                this.classList.add("difficulty");
+                if(this.textContent === "Easy"){
+                    mode = 3;
+                }else{
+                    mode = 6;
+                }
+            restart();
+        });
+    }
+}
+
+function arrayBlock(){
+     for(var i = 0; i<block.length; i++){
+        block[i].addEventListener("click",function(){
+            var clicked = this.style.backgroundColor;
+            if(clicked === picked){
+                gameStatus.textContent = "You're right!!";
+                winnerColor(picked);
+                reset.textContent = "Play Again";
+            }else{
+                this.style.backgroundColor = "#232323";
+                gameStatus.textContent = "Try Again";
+            }
+        
+        });
+    }
+ }
+
+function restart(){
     colors=colorArray(mode);
     picked = pickColor();
     colorPicker.textContent = picked;
+    gameStatus.textContent = "";
+    reset.textContent = "New Colors";
     for(var i = 0; i<block.length; i++){
         if(colors[i]){
+            block[i].style.display = "block";
             block[i].style.backgroundColor = colors[i];
         }else{
-            block[i].style.backgroundColor = "#232323";
+            block[i].style.display = "none";
         }
     }
-    gameStatus.textContent = "";
-    reset.textContent = "New Colors";
-    topBlock.style.backgroundColor = "cadetblue";
-});
-
-hard.addEventListener("click", function(){
-    hard.classList.add("difficulty");
-    easy.classList.remove("difficulty");
-    mode = 6;
-    colors=colorArray(mode);
-    picked = pickColor();
-    colorPicker.textContent = picked;
-    for(var i = 0; i<block.length; i++){
-        block[i].style.backgroundColor = colors[i];
-    }
-    gameStatus.textContent = "";
-    reset.textContent = "New Colors";
-    topBlock.style.backgroundColor = "cadetblue";
-});
-
-for(var i = 0; i<block.length; i++){
-    block[i].style.backgroundColor = colors[i];
-    
-    block[i].addEventListener("click",function(){
-        var clicked = this.style.backgroundColor;
-        if(clicked === picked){
-            gameStatus.textContent = "You're right!!";
-            winnerColor(picked);
-            reset.textContent = "Play Again";
-        }else{
-            this.style.backgroundColor = "#232323";
-            gameStatus.textContent = "Try Again";
-        }
-        
-    });
+    topBlock.style.backgroundColor = "rgb(22, 14, 123)"; /*"cadetblue";*/
 }
+
 
 function pickColor(){
     return colors[Math.floor(Math.random() * colors.length)];
